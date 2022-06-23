09b1500ab9cf887d2f89af93809a5371d818c751
Cristian Apostol <cristian.apostol@btrl.ro>
2022-06-23 15:36:03 +0300

    added base32 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+import { Base32Component } from './components/base32/base32.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'fetch-data', component: FetchDataComponent }
+  { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'base32', component: Base32Component },
 ];
 
 @NgModule({
