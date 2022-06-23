6cd077c2fecd36e1813ebb3e751b166820a55e79
Alin Apostol <alin.cristian977@gmail.com>
2022-06-23 15:40:16 +0300

    added file-upload route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
 import { Base32Component } from './components/base32/base32.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'fetch-data', component: FetchDataComponent },
   { path: 'base32', component: Base32Component },
+  { path: 'file-upload', component: FileUploadComponent }
 ];
 
 @NgModule({
